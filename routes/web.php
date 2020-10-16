56dbad91392174c2f343c7b7986a475ad7f2b7a9
max <enrique.guido@yahoo.com>
2020-10-15 23:57:47 -0400

    Created all new routes for pages

--- routes/web.php
+++ routes/web.php
@@ -21,15 +21,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [\App\Http\Controllers\IndexController::class, 'index']);
 
 // Route for email contact in index.blade
-Route::post('index/send', 'IndexController@send');
+Route::post('index/send', [\App\Http\Controllers\IndexController::class, 'send']);
+// Route::post('index/send', 'IndexController@send');
 
+Route::get('pages/about-me', [\App\Http\Controllers\PagesController::class, 'about']);
+// Route::get('pages/about-me', 'PagesController@about');
 
-Route::get('pages/about-me', 'PagesController@about');
+Route::get('pages/contact', [\App\Http\Controllers\PagesController::class, 'contact']);
+// Route::get('pages/contact', 'PagesController@contact');
 
-Route::get('pages/contact', 'PagesController@contact');
+Route::post('sendemail/send', [\App\Http\Controllers\SendEmailController::class, 'send']);
+// Route::post('sendemail/send', 'SendEmailController@send');
 
-Route::post('sendemail/send', 'SendEmailController@send');
+Route::get('pages/blog-posts', [\App\Http\Controllers\PagesController::class, 'blog']);
+// Route::get('pages/blog-posts', 'PagesController@blog');
 
-Route::get('pages/blog-posts', 'PagesController@blog');
-
-Route::post('subscribe/send', 'SubscribeController@send');
+Route::get('subscribe/send', [\App\Http\Controllers\SubscribeController::class, 'send']);
+// Route::post('subscribe/send', 'SubscribeController@send');
